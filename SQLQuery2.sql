c13a5f68c3e746936332aae81645d5d59572cc87
sohail <sohailqureshi82@gmail.com>
2021-06-29 17:29:39 +0530

    [sohail] Add: Ability to perform Retrieve Particular data using where and select clause

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -13,3 +13,6 @@ values('Terisa', 250000, convert (date,'1999-12-25'));
 
 select * from EmployeePayroll;
 
+select EmployeeName,Salary from EmployeePayroll Where EmployeeName = 'jonny';
+
+select * from EmployeePayroll where StartingDate between CAST('1990-01-01' as date) and CAST('1996-01-01' as date);
