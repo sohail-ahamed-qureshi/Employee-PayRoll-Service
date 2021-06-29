a7670581283791372c1224eb72e88b5918cee759
sohail <sohailqureshi82@gmail.com>
2021-06-29 17:09:26 +0530

    [sohail] Add: Ability to create employee payroll table

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,2 +1,9 @@
 ﻿create database payroll_service;
 use [payroll-service];
+
+create table EmployeePayroll(
+EmployeeId int identity(1,1) primary key,
+EmployeeName varchar(255) not null,
+Salary float not null,
+StartingDate Date not null
+);
